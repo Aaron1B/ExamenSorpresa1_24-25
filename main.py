a1058109ac2a1cb6076cbf2dffad5566aae92062
Aarón Sacristán <asacristanc@student.universidadunie.com>
2025-03-28 09:55:59 +0100

    Cambios 6

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-class punto ():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        if self.x is None:
-            self.x = 0
-        if self.y is None:
-            self.y = 0
-            
-    
+import punto 
+import rectangulo
+
+A = punto.Punto(2, 3)
+B = punto.Punto(5, 5)
+C = punto.Punto(-3, -1)
+D = punto.Punto(0, 0)
+
+print("Punto A:", A)
+print("Punto B:", B)
+print("Punto C:", C)
+print("Punto D:", D)
+
