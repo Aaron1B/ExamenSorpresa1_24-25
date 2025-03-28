c05c472a7944c6134fbe47163a5d0754b1ae6280
Aarón Sacristán <asacristanc@student.universidadunie.com>
2025-03-28 09:50:16 +0100

    Cambios 5

--- rectangulo.py
+++ rectangulo.py
@@ -2,3 +2,12 @@ class Rectangulo:
     def __init__(self, punto_i, punto_f):
         self.punto_inicial = punto_i
         self.punto_final = punto_f
+    def __init__(self, punto_i=(0, 0), punto_f=(0, 0)):
+        self.punto_inicial = punto_i
+        self.punto_final = punto_f
+    def base(self):
+        return abs(self.punto_final[0] - self.punto_inicial[0])
+    def altura(self):
+        return abs(self.punto_final[1] - self.punto_inicial[1])
+    def area(self):
+        return self.base() * self.altura()
