2f6b14ba58c21fef3980bb039777c2524da7ccd0
Aarón Sacristán <asacristanc@student.universidadunie.com>
2025-03-28 09:27:54 +0100

    Cambio1

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ class punto ():
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        if self.x is None:
+            self.x = 0
+        if self.y is None:
+            self.y = 0
     
